358d0a3bc43f3c46ee8d0493ffa9725ab6808c9a
Hadi NW <hadinw@Hadis-MacBook-Pro.local>
2018-08-12 23:45:54 +0700

    update all

--- task15.mengelompokanhewan.js
+++ task15.mengelompokanhewan.js
@@ -1,7 +1,18 @@
 function groupAnimals(animals) {
+  var arrayAnimals = [];
+  var kamus = 'abcdefghijklmnopqrstuvwxyz';
+  var temp=[];
   for (var i = 0; i < animals.length; i++) {
-      console.log(animals[i])
+        arrayAnimals.push(animals[i][0])
+     }
+  
+  for (var i = 0; i < kamus.length; i++) {
+   //console.log(kamus[i])
+   for (var j =0; j < animals.length; j++) {
+    console.log(arrayAnimals[j])
+   }
   }
+  console.log(temp)
 }
 
 // TEST CASES
---
+++ tests.js
@@ -0,0 +1,28 @@
+function groupAnimals(animals) {
+    var arrayAnimals = [];
+    var kamus = 'abcdefghijklmnopqrstuvwxyz';
+    var temp=[];
+    var result = [];
+    for (var i = 0; i < kamus.length; i++) {
+        for (var j = 0; j < animals.length; j++) {
+           if (kamus[i] === animals[j][0]) {
+               temp.push([animals[j]])  
+                            
+           } 
+        }
+        // result.push(temp);
+        // temp = [];   
+       
+    }
+    //console.log(temp)
+    return temp;
+   
+    
+    
+  }
+  
+  // TEST CASES
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+  //console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
