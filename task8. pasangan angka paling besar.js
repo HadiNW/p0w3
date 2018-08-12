be290017095c4495797c68e43b8529da4477f62e
Hadi NW <hadinw@Hadis-MacBook-Pro.local>
2018-08-12 17:42:50 +0700

    a

--- task8. pasangan angka paling besar.js
+++ task8. pasangan angka paling besar.js
@@ -1,12 +1,39 @@
 function pasanganTerbesar(num) {
-  for(var i = 0; i < num.length; i++) {
-      console.log(1);
+  var angka = num.toString();
+  var numArray = [];
+  var angkaString = '';
+  for (var i = 0; i < angka.length; i++) {   
+     angkaString += angka[i]
+     if (i % 2 !== 0 ) {
+       numArray.push(angkaString)
+       angkaString = ''
+     } 
   }
-}
+  
+  //console.log(numArray) 
+  var done = false;
+  while (done === false) {
+    done = true;
+    for (var i = 1; i < numArray.length; i += 1) {
+      if (numArray[i - 1] > numArray[i]) {
+        done = false;
+        var tmp = numArray[i - 1];
+        numArray[i - 1] = numArray[i];
+        numArray[i] = tmp;
+      }
+    }
+  }
+  
+  numArray = numArray[numArray.length-1]
+  return Number(numArray);
 
+}    
+ // 0 1 2
+
+//pasanganTerbesar(641573)
 // TEST CASES
-pasanganTerbesar(641573); // 73
-// console.log(pasanganTerbesar(12783456)); // 83
-// console.log(pasanganTerbesar(910233)); // 91
-// console.log(pasanganTerbesar(71856421)); // 85
-// console.log(pasanganTerbesar(79918293)); // 99
+console.log(pasanganTerbesar(641573)); // 73
+console.log(pasanganTerbesar(12783456)); // 83
+console.log(pasanganTerbesar(910233)); // 91
+console.log(pasanganTerbesar(71856421)); // 85
+console.log(pasanganTerbesar(79918293)); // 99
---
+++ test.js
@@ -0,0 +1,32 @@
+function bubbleSort(array) {
+  var done = false;
+  while (done === false) {
+    done = true;
+    for (var i = 1; i < array.length; i += 1) {
+      if (array[i - 1] > array[i]) {
+        done = false;
+        var tmp = array[i - 1];
+        array[i - 1] = array[i];
+        array[i] = tmp;
+      }
+    }
+  }
+
+  return array;
+}
+
+var numbers = [12, 10, 15, 11, 14, 13, 16];
+bubbleSort(numbers);
+console.log(numbers);
+
+
+
+
+
+
+// TEST CASES
+// console.log(pasanganTerbesar(641573)); // 73
+// console.log(pasanganTerbesar(12783456)); // 83
+// console.log(pasanganTerbesar(910233)); // 91
+// console.log(pasanganTerbesar(71856421)); // 85
+// console.log(pasanganTerbesar(79918293)); // 99
